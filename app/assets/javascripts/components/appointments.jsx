808a4cd116264a4d0ade3f1451171683bf819616
Sonam Jagetia <sonamvjagetia@gmail.com>
2018-07-24 21:43:44 +0530

    Handle user form input using React state

--- app/assets/javascripts/components/appointments.jsx
+++ app/assets/javascripts/components/appointments.jsx
@@ -1,9 +1,23 @@
 var Appointments = createReactClass({
+  getInitialState: function() {
+    return {
+      appointments: this.props.appointments,
+      input_title: 'Team standup meeting',
+      input_appt_time: 'Tomorrow at 9 am'
+    }
+  },
+
+  handleUserInput: function(obj) {
+    this.setState(obj);
+  },
+
   render: function() {
     return (
       <div>
-        <AppointmentForm />
-        <AppointmentList appointments={this.props.appointments} />
+        <AppointmentForm input_title={this.state.title}
+          input_appt_time={this.state.appt_time}
+          onUserInput={this.handleUserInput} />
+        <AppointmentList appointments={this.state.appointments} />
       </div>
     )
   }
